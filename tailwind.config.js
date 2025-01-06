f0eec67946f3fb8e570ba95588b0db71db23ea0f
Anna Shirinskaya <av.shirinskaya@gmail.com>
2025-01-06 18:19:30 +0100

    fix: update index.css, index.html, package.json, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: colors.neutral[700],
+        },
+        // secondary: { ...colors.blue, DEFAULT: colors.blue[300] },
+        transparent: "transparent",
+        current: "currentColor",
+        accent: {
+          DEFAULT: colors.red[600],
+          gray: colors.neutral[200],
+          green: "#A3D29D",
+        },
+      },
+    },
   },
   plugins: [],
 };
