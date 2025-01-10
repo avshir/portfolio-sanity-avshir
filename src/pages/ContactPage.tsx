c5a04e0bad40f877d417d36fcc6e8e6e1d7da915
Anna Shirinskaya <av.shirinskaya@gmail.com>
2025-01-10 18:07:30 +0100

    feat: add ContactPage

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,11 +1,27 @@
 import { ContainerMain } from "../components/ContainerMain";
 import { Container } from "../components/Container";
+import { ContactForm } from "../components/ContactForm";
+import Map from "./../components/Map";
 
 const ContactPage = () => {
   return (
-    <ContainerMain>
+    <ContainerMain className="pb-2">
       <Container>
-        <h1 className="text-accent">ContactPage</h1>
+        <div className="grid lg:grid-cols-2 md:divide-x gap-4">
+          <div className="max-w-md mx-auto lg:ml-auto lg:mr-0 lg:pr-6">
+            <h1 className="text-accent">Contact me</h1>
+            <p>
+              I am interested in work full time as a frontend developer remotely
+              or hybrid in Poznan, Poland or freelance opportunities. However,
+              if you have other request or question, don’t hesitate to contact
+              me using below form either.
+            </p>
+            <ContactForm />
+          </div>
+          <div className="relative lg:pl-6 flex items-center">
+            <Map />
+          </div>
+        </div>
       </Container>
     </ContainerMain>
   );
