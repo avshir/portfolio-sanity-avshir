ba78a430c93b7836fe0248d6a8d69fdcdd2bca98
Anna Shirinskaya <av.shirinskaya@gmail.com>
2025-01-16 18:17:43 +0100

    feat: use local variables - VITE_EMAIL_ACCESS_KEY, hide Email Key

--- src/constants/email_key.ts
+++ src/constants/email_key.ts
@@ -1,4 +1,4 @@
 // web3forms.com for Contact Form
 // Public Access Key is:
 // for email av.shirinskaya@gmail.com:
-export const EMAIL_ACCESS_KEY = "ab2675fb-ad1f-4bf6-93d9-bf1ff1ed08af";
+export const EMAIL_ACCESS_KEY = import.meta.env.VITE_EMAIL_ACCESS_KEY;
